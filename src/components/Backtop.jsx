48964cbe504088ba77321a3785b1b652b05338b0
prince-372 <pmor18314@gmail.com>
2024-01-25 12:38:54 +0530

    change shape of icon

--- src/components/Backtop.jsx
+++ src/components/Backtop.jsx
@@ -26,7 +26,7 @@ const BackToTop = () => {
                 <div onClick={() => setPosition({ ...position, position: { top: 0, left: 0 } })} className="fixed hidden bottom-[3%] right-[2%] z-30 translate-y-[0%] animate-bounce"
                     ref={scrollTop}
                 ><div className='h-[52px] w-[52px] rounded-[50%] border-[]  cursor-pointer scroll-smooth transform-[all] transform-[.3s] transform-[linear]  flex justify-center items-center'>
-                        <img src={Backbtn} alt="" />
+                        <img className=' w-[100px]' src={Backbtn} alt="" />
                     </div></div>
             </div>
 
